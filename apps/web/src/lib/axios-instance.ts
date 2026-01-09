ea5d601601cce1da99ad29c2519816cfaf8d690c
compyneller@gmail.com <compyneller@gmail.com>
2026-01-09 15:33:29 +0530

    better auth base url change

--- apps/web/src/lib/axios-instance.ts
+++ apps/web/src/lib/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  // baseURL: process.env.BASE_URL || "http://localhost:5001",
-  baseURL: "https://api.bagalkidukaan.store",
+  baseURL: process.env.BASE_URL || "http://localhost:5001",
+  // baseURL: "https://api.bagalkidukaan.store",
 
   withCredentials: true,
 });
